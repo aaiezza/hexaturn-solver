1e94ef0ae3b9be3ac7a61b819e60d7f3ead724c5
Alex Aiezza <alex.aiezza@daveramsey.com>
2019-02-23 13:31:13 -0500

    Add DecoyGoal to model

---
+++ hexaturn-model/src/main/java/com/shaba/hexaturn/DecoyGoal.java
@@ -0,0 +1,12 @@
+/**
+ *  COPYRIGHT (C) 2015 Alessandro Aiezza II. All Rights Reserved.
+ */
+package com.shaba.hexaturn;
+
+/**
+ * @author Alessandro Aiezza II
+ *
+ */
+@lombok.Data
+public class DecoyGoal implements EnemyAttractingOccupant
+{}
